83eb2e564cf15ffedfabf218337dc92cb23e70ec
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-13 16:27:16 +0300

    Seed notes table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\User;
 
 use App\Notebook;
 
+use App\Note;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -17,9 +19,15 @@ class DatabaseSeeder extends Seeder
     {
         User::truncate();
         Notebook::truncate();
+        Note::truncate();
 
         factory(App\User::class, 5)->create()->each(function($u) {
         	$u->notebooks()->save(factory(App\Notebook::class)->make());
         });
+
+        $notebooks = Notebook::all();
+        foreach ($notebooks as $notebook) {
+            $notebook->notes()->save(factory(App\Note::class)->make());
+        }
     }
 }
