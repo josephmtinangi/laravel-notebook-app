ad674a7d1b36208107e92821cd924d02be06b048
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-08 22:27:34 +0300

    Add jumbotron to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
-
+<div class="jumbotron" style="background: #fff; color: #999;">
+	<div class="container">
+		<h1>Notebook App</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. 
+		</p>
+		<p>
+			<a class="btn btn-primary btn-lg">Learn more</a>
+		</p>
+	</div>
+</div>
 
 @endsection
